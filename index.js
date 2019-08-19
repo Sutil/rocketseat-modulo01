86aafa87ffab8bcde993a892bcc99444465eaf05
Eduardo <sutil.edu@gmail.com>
2019-08-19 14:33:45 -0300

    First commit - hello world

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const server = express();
+
+server.get('/teste', (req, res) => {
+    console.log('teste');
+
+    return res.json({ message: 'Hello World'});
+});
+
+server.listen(3000);
