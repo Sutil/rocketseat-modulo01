c06c45744e83a791cc21ba00c37a4d42155e0706
Eduardo <sutil.edu@gmail.com>
2019-08-19 20:46:17 -0300

    Criando middlewares de interceptação de requisição, tanto globais como locais.

--- index.js
+++ index.js
@@ -6,17 +6,45 @@ server.use(express.json());
 
 const users = ['Diego', 'Cláudio', 'Sutil'];
 
-server.get('/users/:index', (req, res) => {
-    const { index } =  req.params;
+server.use((req, res, next)  => {
+    console.time('Request');
+    console.log(`Método: ${req.method}; URL: ${req.url}`);
 
-    return res.json(users[index]);
+    next();
+
+    console.timeEnd('Request');
 });
+
+function checkUserExists(req, res, next) {
+    if(!req.body.name) {
+        return res.status(400).json({ error: 'User name is required'});
+    }
+
+    return next();
+}
+
+function checkUserInArray(req, res, next) {
+    const { index } = req.params;
+    const user = users[index];
+
+    if(!users) {
+        return res.status(400).json({ error: 'User does not exists'});
+    }
+
+    req.user = user;
+
+    return next();
+}
 
 server.get('/users', (req, res) => {
     return res.json(users);
 });
 
-server.post('/users', (req, res) => {
+server.get('/users/:index', checkUserInArray, (req, res) => {
+    return res.json(req.user);
+});
+
+server.post('/users', checkUserExists, (req, res) => {
     const { name } = req.body;
     
     users.push(name);
@@ -24,7 +52,7 @@ server.post('/users', (req, res) => {
     return res.json(users);
 });
 
-server.put('/users/:index', (req, res) => {
+server.put('/users/:index', checkUserInArray, checkUserExists, (req, res) => {
     const { index } = req.params;
     const { name } = req.body;
 
@@ -33,7 +61,7 @@ server.put('/users/:index', (req, res) => {
     return res.json(users);
 });
 
-server.delete('/users/:index', (req, res) => {
+server.delete('/users/:index', checkUserInArray, (req, res) => {
     const { index } = req.params;
 
     users.splice(index, 1);
